53bc43f56b7e61e2fe04afdea5b828c5cbe27b1c
Wescott Sharples <wesc@ttsharpl.es>
2019-04-21 11:37:21 -0700

    Add SQLALchemy and some entities

---
+++ src/entities/exam.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+from sqlalchemy import Column, String
+
+from .entity import Entity, Base
+
+
+class Exam(Entity, Base):
+    __tablename__ = 'exams'
+
+    title = Column(String)
+    description = Column(String)
+
+    def __init__(self, title, description, created_by):
+        Entity.__init__(self, created_by)
+        self.title = title
+        self.description = description
